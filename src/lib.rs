c566015daa95666e284bc2d08237e097dabc72fb
Richard Harris <rjh@netcraft.com>
2017-04-09 18:04:25 +0100

    Add SplitLine, an object to hold the fields of a line.
    This is basically owning_ref but just for my use case, and has
    pretty methods. Getting the clone to work was fun!

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,53 @@ use std::cmp::Ordering;
 //    * Doesn't handle (or detect) cross joins properly
 //    * Doesn't detect incorrect ordering of files
 
+// Vec<&str> of fields in a string, with string also kept as backing storage.
+// Must use raw pointers as Rust doesn't storing X and &(part of X) together
+struct SplitLine {
+    line: String,
+    fields: Vec<*const str>,
+    key_field: usize,
+}
+
+impl SplitLine {
+    fn new(line: String, delim: char, key_field: usize) -> Self {
+        let fields : Vec<*const str> = line.split(delim).map(|x| x as *const str).collect();
+
+        SplitLine { line, fields, key_field }
+    }
+
+    fn field(&self, index: usize) -> &str {
+        unsafe { &*self.fields[index] }
+    }
+
+    fn key(&self) -> &str {
+        self.field(self.key_field)
+    }
+}
+
+// Clone requires us to clone the underlying string. We reuse the offsets
+impl Clone for SplitLine {
+    fn clone(&self) -> Self {
+        // Get start of original line (String -> &str -> *const u8)
+        let origstart : *const u8 = self.line.as_bytes().as_ptr();
+
+        // Clone original line
+        let newline = self.line.clone();
+
+        // Generate a new set of fields by calculating the original offsets and adding
+        // them to the new string's pointer.
+        let newfields : Vec<*const str> = self.fields.iter().map(|ptr| {
+            unsafe {
+                let s : &str = &**ptr;
+                let offset : usize = s.as_ptr() as usize - origstart as usize;
+                newline.slice_unchecked( offset, offset + s.len() ) as *const str
+            }
+        }).collect();
+
+        SplitLine { line: newline, fields: newfields, key_field: self.key_field }
+    }
+}
+
 type LineIterator = Iterator<Item=io::Result<String>>;
 
 pub struct JoinConfig {
@@ -55,8 +102,8 @@ struct JoinFile {
     row: String,
     key: String,
     printed: bool,
-    next_key: String,
     next_row: String,
+    next_key: String,
 }
 
 impl JoinFile {
@@ -266,3 +313,24 @@ fn smart_refill(left: &mut JoinFile, right: &mut JoinFile) -> bool {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let s = SplitLine::new("foo\tbar\tbaz".into(), '\t', 1);
+        assert_eq!(s.field(0), "foo");
+        assert_eq!(s.field(1), "bar");
+        assert_eq!(s.field(2), "baz");
+        assert_eq!(s.key(), "bar");
+
+        let t = s.clone();
+        assert_eq!(t.field(0), "foo");
+        assert_eq!(t.field(1), "bar");
+        assert_eq!(t.field(2), "baz");
+        assert_eq!(t.key(), "bar");
+    }
+}
+
